ec22fcbea751d8ebac8585aabbd9ece43de4e87d
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2014-05-08 23:20:08 +0530

    first commit

---
+++ GitHubDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace GitHubDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GitHubDemo");
+            Console.WriteLine("Team Explorer");
+        }
+    }
+}
